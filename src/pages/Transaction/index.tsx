a9542bf11206772013c2dd03821db8f7e0537c63
José Jorge <jorgesouza621@gmail.com>
2024-07-24 10:15:49 -0300

    Estilização da Table

--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighLight,
+  TransActionContainer,
+  TransActionTable,
+} from './styles'
 
 export function Transaction() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransActionContainer>
+        <TransActionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td> 12/04/2024</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="outcome">R$ -59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td> 12/04/2024</td>
+            </tr>
+          </tbody>
+        </TransActionTable>
+      </TransActionContainer>
     </div>
   )
 }
