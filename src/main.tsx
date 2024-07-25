0ef97ba5eddda5de9e2d8a75b5068f91f1da7a75
José Jorge <jorgesouza621@gmail.com>
2024-07-25 18:50:57 -0300

    Utilizando a emotion para resolver problema do styled components (prop variant)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { StyleSheetManager } from 'styled-components'
+import emotionIsPropValid from '@emotion/is-prop-valid'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StyleSheetManager shouldForwardProp={emotionIsPropValid}>
+      <App />
+    </StyleSheetManager>
   </React.StrictMode>,
 )
